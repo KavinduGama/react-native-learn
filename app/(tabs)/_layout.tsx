2bdfce1ae306b1ec2c036f9a440a1c2477108860
Kavindu Deshappriya <kavindu@kayatech.com>
2025-06-07 13:31:44 +0530

    routing 1.0

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+export default function TabsLayout() {
+  return (
+  <Tabs screenOptions={{tabBarActiveTintColor:"coral"}}>
+    <Tabs.Screen name="index" options={{title : "Home", tabBarIcon : ({color}) =>(
+      <FontAwesome name="home" size={24} color={color} />
+    )}}/>
+    <Tabs.Screen name="login" options={{title : "Login"}}/>
+  </Tabs>
+  )
+}
